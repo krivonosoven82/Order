a5d2b0368fb81334ce3efb6b4b4727a97c62041e
krivonosoven82 <krivonosoven82@gmail.com>
2018-03-07 18:08:38 +0200

    CabinetAccount

---
+++ CabinetAccount/app/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from'react-dom';
+import CabinetAccount from './components/CabinetAccount.js';
+
+
+ReactDOM.render(
+  <CabinetAccount />, document.getElementById('root')
+);
