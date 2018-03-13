7bdb557a8a792d556e01a8f30610544baff90fb6
krivonosoven82 <krivonosoven82@gmail.com>
2018-03-13 13:11:56 +0200

    dev3

--- CabinetAccount/app/app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from'react-dom';
-import CabinetAccount from './components/CabinetAccount.js';
-
-
-ReactDOM.render(
-  <CabinetAccount />, document.getElementById('root')
-);
